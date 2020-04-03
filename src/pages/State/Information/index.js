749a18490e9346eb168aa30715e966960a9b9900
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-04-03 00:46:17 -0300

    pages: state information - not worked

--- src/pages/State/Information/index.js
+++ src/pages/State/Information/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+import api from '~/services/api';
+
 const InformationStatePage = () => {
-  const { state } = useParams();
+  const { uf } = useParams();
+
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    async function getStates() {
+      // const { data } = await api.get('').then((r) => r.data);
+      // setStates(data);
+    }
+
+    getStates();
+  }, []);
 
   return <h1>InformationStatePage</h1>;
 };
