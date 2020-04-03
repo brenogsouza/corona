4a7776222bcb5cc0589c819d602c90bb60464d78
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-04-03 01:14:24 -0300

    feature: now sorted alphabetically

--- src/pages/State/Information/index.js
+++ src/pages/State/Information/index.js
@@ -1,20 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 
 import api from '~/services/api';
 
 const InformationStatePage = () => {
+  const history = useHistory();
   const { uf } = useParams();
 
   const [state, setState] = useState([]);
 
   useEffect(() => {
-    async function getStates() {
-      // const { data } = await api.get('').then((r) => r.data);
-      // setStates(data);
+    async function getState() {
+      const data = await api.get(`brazil/uf/${uf}`).then((r) => r.data);
+
+      if (data.error) history.push('/state');
+      else setState(data);
     }
 
-    getStates();
+    getState();
   }, []);
 
   return <h1>InformationStatePage</h1>;
