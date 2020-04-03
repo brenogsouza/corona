37e00476c5c1206d40f127581dcd9475bee1465e
Gabriel "Dev HijaZi" F <46979459+devhijazi@users.noreply.github.com>
2020-04-03 01:26:39 -0300

    World Page
    Estilização

--- src/pages/World/index.js
+++ src/pages/World/index.js
@@ -2,15 +2,40 @@ import React from 'react';
 
 import Layout from '~/layouts/Information';
 
-import { Container, Content } from './styles';
+import {
+  Container,
+  HeaderSection,
+  GlobalCount,
+  Section,
+  ContentSection,
+} from './styles';
 
 const WorldPage = () => (
   <Layout>
     <Container>
-      <Content>
-        <h1>COVID-19</h1>
-        <span>Dados Mundiais</span>
-      </Content>
+      <HeaderSection>
+        <h1>COVID-19 - MUNDO</h1>
+      </HeaderSection>
+
+      <GlobalCount>
+        <h1>0</h1>
+        <p>INFECTADOS</p>
+      </GlobalCount>
+      <Section>
+        <ContentSection>
+          <h1>0</h1>
+          <p>ATIVOS</p>
+        </ContentSection>
+        <ContentSection>
+          <h1>0</h1>
+          <p>MORTES</p>
+        </ContentSection>
+        <ContentSection>
+          <h1>0</h1>
+          <p>CURAS</p>
+        </ContentSection>
+      </Section>
+      <h3>Última Atualização : 00/00/0000 - 00:00</h3>
     </Container>
   </Layout>
 );
