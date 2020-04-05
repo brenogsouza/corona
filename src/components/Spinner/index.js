9913c516deae79fbf60310ba4b2307a5c7c79917
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-04-05 01:12:01 -0300

    components: spinner

---
+++ src/components/Spinner/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const SpinnerComponent = () => <Container />;
+
+const spinner = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const Container = styled.div`
+  width: 60px;
+  height: 60px;
+
+  border-radius: 50%;
+  border: 3px solid transparent;
+  border-top: 3px solid #0353a4;
+  border-left: 3px solid #0353a4;
+
+  animation: ${spinner} 1s linear infinite;
+`;
+
+export default SpinnerComponent;
