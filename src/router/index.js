7ce6e9c0f658d9bbaa919e150d7e2b9003957e72
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-04-05 16:52:07 -0300

    routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
+import BrazilPage from '~/pages/Brazil';
 import DownloadPage from '~/pages/Download';
 import HomePage from '~/pages/Home';
 import NewsPage from '~/pages/News';
+import NotFoundPage from '~/pages/NotFound';
 import StatePage from '~/pages/State';
 import InformationStatePage from '~/pages/State/Information';
 import WorldPage from '~/pages/World';
@@ -14,11 +16,14 @@ const Routes = () => (
       <Route path="/" component={HomePage} exact />
 
       <Route path="/world" component={WorldPage} />
+      <Route path="/brazil" component={BrazilPage} />
       <Route path="/state" component={StatePage} exact />
       <Route path="/state/:uf" component={InformationStatePage} />
 
       <Route path="/news" component={NewsPage} />
       <Route path="/download" component={DownloadPage} />
+
+      <Route path="/404" component={NotFoundPage} />
 
       <Redirect to="/404" />
     </Switch>
