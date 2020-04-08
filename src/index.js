3008151525bed9b33c540e7b9692d34074ec7753
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-04-07 22:21:59 -0300

    fixes

--- src/index.js
+++ src/index.js
@@ -4,8 +4,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import * as ServiceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-ServiceWorker.register();
